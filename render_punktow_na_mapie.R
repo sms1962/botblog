43ea335780ecbc0ae88a29267bdd457f97f54c4b
Stanislaw M. Stanuch <stanuch+github@gmail.com>
2021-05-10 23:19:01 +0200

    Zmiana docelowego pliku

--- render_punktow_na_mapie.R
+++ render_punktow_na_mapie.R
@@ -1,4 +1,4 @@
 # render mapy punktów
 library(rmarkdown)
 library(distill)
-rmarkdown::render("mapa_punktow_botkrakow.Rmd")
+rmarkdown::render("mapa_punktow_botkrakow.Rmd", output_file = "./_site/mapa_punktow_botkrakow.html")
