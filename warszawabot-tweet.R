173e0fe5d3afef85858268d1203d5684c2b550bc
Stanislaw M. Stanuch <stanuch+github@gmail.com>
2021-05-11 15:01:07 +0200

    ostatnia próba

--- warszawabot-tweet.R
+++ warszawabot-tweet.R
@@ -54,35 +54,35 @@ rtweet::post_tweet(
   token = warszawabot_token
 )
 
-# # pobranie ostatnich twitow (max 100)
-# df <- get_timeline(user = "warszawabot", n=500, token = warszawabot_token)
-# 
-# df_dzisiejsze <- df %>% 
-#   unnest(ext_media_url) %>% 
-#   # filter(created_at >= ymd(Sys.Date(), tz="Europe/Warsaw")) %>%
-#   select(user_id, status_id, created_at, text, favorite_count, retweet_count, followers_count, statuses_count, 
-#          ext_media_url) %>% 
-#   mutate_all(as.character)
-# 
-# 
-# # pobranie aktualnego loga
-# 
-# log_data <- read_csv("./data/warszawabot_archiwum_twitow.csv", 
-#                      col_types = cols(user_id = col_character(), 
-#                                       status_id = col_character(), created_at = col_character(),
-#                                       favorite_count = col_character(), retweet_count = col_character(),
-#                                       followers_count = col_character(), statuses_count = col_character(),
-#                                       ext_media_url = col_character()))
-# 
-# # wybieram wiersze do archiwzacji
-# 
-# log_new_data <- anti_join(df_dzisiejsze, log_data, by = "status_id")
-# 
-# # dodanie do istniejącego loga na dysku nowych twitow
-# 
-# write_csv(log_new_data,"./data/warszawabot_archiwum_twitow.csv",append = TRUE)
-# # write_csv(df_dzisiejsze,"./data/archiwum_twitow.csv")
-# 
-# # Generowanie mapy aktualnych punktów (dawne mapa_punktów_botkrakow.RMD)
-# 
-# rmarkdown::render("mapa_punktow_warszawabot.Rmd", output_file = "./_site/mapa_punktow_warszawabot.html")
+# pobranie ostatnich twitow (max 100)
+df <- get_timeline(user = "warszawabot", n=500, token = warszawabot_token)
+
+df_dzisiejsze <- df %>%
+  unnest(ext_media_url) %>%
+  # filter(created_at >= ymd(Sys.Date(), tz="Europe/Warsaw")) %>%
+  select(user_id, status_id, created_at, text, favorite_count, retweet_count, followers_count, statuses_count,
+         ext_media_url) %>%
+  mutate_all(as.character)
+
+
+# pobranie aktualnego loga
+
+log_data <- read_csv("./data/warszawabot_archiwum_twitow.csv",
+                     col_types = cols(user_id = col_character(),
+                                      status_id = col_character(), created_at = col_character(),
+                                      favorite_count = col_character(), retweet_count = col_character(),
+                                      followers_count = col_character(), statuses_count = col_character(),
+                                      ext_media_url = col_character()))
+
+# wybieram wiersze do archiwzacji
+
+log_new_data <- anti_join(df_dzisiejsze, log_data, by = "status_id")
+
+# dodanie do istniejącego loga na dysku nowych twitow
+
+write_csv(log_new_data,"./data/warszawabot_archiwum_twitow.csv",append = TRUE)
+# write_csv(df_dzisiejsze,"./data/archiwum_twitow.csv")
+
+# Generowanie mapy aktualnych punktów (dawne mapa_punktów_botkrakow.RMD)
+
+rmarkdown::render("mapa_punktow_warszawabot.Rmd", output_file = "./_site/mapa_punktow_warszawabot.html")
